fddb4cb73358279a12bfd3d6cc841c5e05b267b0
khalidjambi <khalid.jambi18@gmail.com>
2019-02-20 14:15:03 +0700

    tipe

---
+++ main.js
@@ -0,0 +1,20 @@
+//Deklarasi Variable
+//-------------------
+
+//Nama dimulai dengan huruf atau $ atau _
+//Nama boleh mengandung huruf,angka,underscore _ dan $ dollar
+//Nama itu case sensitive 
+//Ada 'reserved words' yang tidak boleh dipakai
+
+// tipe data string
+
+var text = "Sekolah Koding";
+
+
+/*
+1.memilih id wrap
+2.metode innerHTML = nulis konten
+*/
+
+
+document.getElementById("wrap").innerHTML = text;
